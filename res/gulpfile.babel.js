32099491fa89fd290bbb24063efa76d41920b493
enixdark <cqshinn92@gmail.com>
2016-02-16 02:09:58 +0700

    init prj with gulp use es6

---
+++ res/gulpfile.babel.js
@@ -0,0 +1,15 @@
+// var gulp = require('gulp');
+// var browserify = require('browserify');
+// var babelify = require('babelify')
+// var source = require('vinyl-source-stream');
+
+// gulp.task('default', function(){
+// 	console.log('I am about to learn the essentials of React.js');
+// })
+'use strict';
+
+import gulp from 'gulp';
+import browserify from 'browserify';
+import babelify from 'babelify';
+import source from 'vinyl-source-stream';
+gulp.task('default', () => console.log('Default task called'));
